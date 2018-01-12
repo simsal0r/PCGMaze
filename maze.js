19f48ab36f724da6fc3f74b5cf5171f53376a6d4
Simeon <simeonbrueggenjuergen@gmail.com>
2018-01-12 10:46:20 +0100

    Merge remote-tracking branch 'remotes/origin/Laura' into simeon-test
    # Conflicts:
    #	maze.js
    
    Integrate Lauras Algorithm in Menu

--- maze.js
+++ maze.js
@@ -1,5 +1,5 @@
 
-function generateSquareMaze(dimension) {
+function alg_Laura(dimension) {
 
     //field = growingtree.create(dimension, dimension, 20);
     //field.dimension = dimension;
@@ -130,4 +130,58 @@ function generateSquareMaze(dimension) {
 
 }
 
+//Astray standard
+function generateSquareMaze(dimension) {
 
+    function iterate(field, x, y) {
+        field[x][y] = false;
+        while(true) {
+            directions = [];
+            if(x > 1 && field[x-2][y] == true) {
+                directions.push([-1, 0]);
+            }
+            if(x < field.dimension - 2 && field[x+2][y] == true) {
+                directions.push([1, 0]);
+            }
+            if(y > 1 && field[x][y-2] == true) {
+                directions.push([0, -1]);
+            }
+            if(y < field.dimension - 2 && field[x][y+2] == true) {
+                directions.push([0, 1]);
+            }
+            if(directions.length == 0) {
+                return field;
+            }
+            dir = directions[Math.floor(Math.random()*directions.length)];
+            field[x+dir[0]][y+dir[1]] = false;
+            field = iterate(field, x+dir[0]*2, y+dir[1]*2);
+        }
+    }
+
+    // Initialize the field.
+    var field = new Array(dimension);
+    field.dimension = dimension;
+    for(var i = 0; i < dimension; i++) {
+        field[i] = new Array(dimension);
+        for (var j = 0; j < dimension; j++) {
+            field[i][j] = true;
+        }
+    }
+
+    // Gnerate the maze recursively.
+    field = iterate(field, 1, 1);
+
+    return field;
+
+
+}
+
+//does not really work
+function generateGrowingTreeMaze(dimension) {
+
+    field = growingtree.create(dimension, dimension, 20);
+    field.dimension = dimension;
+
+    return field;
+
+}
